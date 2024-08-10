0a2198115fc7148e55660a4e48380597a8a102fd
Clayton Townsend II <clayton@claytontii.com>
2024-08-10 17:50:31 -0500

    move node cache

--- interface.go
+++ interface.go
@@ -10,43 +10,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
-	"github.com/simplylib/genericsync"
 )
-
-type Node struct {
-	*Resource
-}
-
-// NodeCache is a non-optimal cache that does cache things
-// but sometimes multiple requests for the same object will result in
-// multiple filebrowser request, but eventually will keep them in cache
-type NodeCache struct {
-	sess *filebrowserSession
-
-	// Cache of map[ID string]Node
-	cache genericsync.Map[string, Node]
-}
-
-func NewNodeCache(sess *filebrowserSession) *NodeCache {
-	return &NodeCache{sess: sess}
-}
-
-func (nc *NodeCache) Info(ctx context.Context, path string) (*Resource, error) {
-	node, ok := nc.cache.Load(path)
-	if ok {
-		return node.Resource, nil
-	}
-
-	res, err := nc.sess.Info(ctx, path)
-	if err != nil {
-		return nil, err
-	}
-
-	nc.cache.Store(path, Node{Resource: res})
-	slog.Debug("caching resource info", "path", path)
-
-	return res, nil
-}
 
 func browse(w fyne.Window, sess *filebrowserSession) {
 	cache := NewNodeCache(sess)
---
+++ nodecache.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/simplylib/genericsync"
+)
+
+type Node struct {
+	*Resource
+}
+
+// NodeCache is a non-optimal cache that does cache things
+// but sometimes multiple requests for the same object will result in
+// multiple filebrowser request, but eventually will keep them in cache
+type NodeCache struct {
+	sess *filebrowserSession
+
+	// Cache of map[ID string]Node
+	cache genericsync.Map[string, Node]
+}
+
+func NewNodeCache(sess *filebrowserSession) *NodeCache {
+	return &NodeCache{sess: sess}
+}
+
+func (nc *NodeCache) Info(ctx context.Context, path string) (*Resource, error) {
+	node, ok := nc.cache.Load(path)
+	if ok {
+		return node.Resource, nil
+	}
+
+	res, err := nc.sess.Info(ctx, path)
+	if err != nil {
+		return nil, err
+	}
+
+	nc.cache.Store(path, Node{Resource: res})
+	slog.Debug("caching resource info", "path", path)
+
+	return res, nil
+}
