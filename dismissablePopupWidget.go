11768c37bbcd954f4fe88188775a16ae0d389c0d
Clayton Townsend II <clayton@claytontii.com>
2024-08-10 17:23:42 -0500

    add a dismissable popup widget set with text

---
+++ dismissablePopupWidget.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"sync"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func ShowDismissablePopup(window fyne.Window, msg string) {
+	once := sync.Once{}
+
+	var modal *widget.PopUp
+
+	label := widget.NewLabel(msg)
+	scroll := container.NewHScroll(label)
+
+	msgWidget := container.NewVBox(
+		scroll,
+		widget.NewButton("Copy Text", func() {
+			window.Clipboard().SetContent(msg)
+		}),
+		widget.NewButton("Okay", func() {
+			once.Do(modal.Hide)
+		}),
+	)
+
+	modal = widget.NewModalPopUp(msgWidget, window.Canvas())
+	modal.Show()
+
+	modal.Resize(fyne.NewSize(window.Canvas().Size().Width, modal.Size().Height/2))
+}
